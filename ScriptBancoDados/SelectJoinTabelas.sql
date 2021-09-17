2565f16f722bd58b73abd44c3e9604dfc04f921d
Giovanni Pratto <giovanni.pratto@ufms.br>
2021-09-17 17:34:07 -0400

    Script testados

---
+++ ScriptBancoDados/SelectJoinTabelas.sql
@@ -0,0 +1,12 @@
+select 
+P.CategoriaID,
+C.Descricao as CategoriaDescrição,
+P.SubCategoriaID,
+S.Descricao as SubCategoriaDescrição,
+P.ProdutoID,
+P.Descricao as ProdutoDescrição
+from Produto as P
+INNER JOIN Categoria as C ON P.CategoriaID =  C.CategoriaID
+INNER JOIN Subcategoria as S on P.SubCategoriaID = S.SubCategoriaID
+where (P.CategoriaID = 10) and (P.SubCategoriaID = 65 )
+go
